a1c99c78051041cfd3a4b67aee3397a7e048b738
shawnaClimer <shawnaclimer@gmail.com>
2017-02-02 12:59:25 -0600

    Add files via upload
    used code from book to flesh out functions
    also added structure to main

--- loglib.c
+++ loglib.c
@@ -11,22 +11,45 @@ typedef struct list_struct {
 static log_t *headptr = NULL;
 static log_t *tailptr = NULL;
 
-int addmsg(data_t data, const char *ermsg) {
-	//build string to add to log
-	char *message[200];
-	int success = buildmsg(data, message, ermsg);
-	if(success == -1){
+int addmsg(data_t data) {
+	//allocates node for data and adds to end of list
+	log_t *newnode;
+	int nodesize;
+	nodesize = sizeof(log_t) + strlen(data.string) + 1;
+	//try to add node
+	if((newnode = (log_t *)(malloc(nodesize))) == NULL){
 		return -1;
-	}else {
-	//need to add message to list
-	
-		
-		return 0;
 	}
+	newnode->item.time = data.time;
+	newnode->item.string = (char *)newnode + sizeof(log_t);
+	strcpy(newnode->item.string, data.string);
+	newnode->next = NULL;
+	if (headptr == NULL){
+		headptr = newnode;//list was empty
+	}else{
+		tailptr->next = newnode;//adds to end of list
+	}
+	tailptr = newnode;
+	return 0;
+	
 }
 
-int buildmsg(data_t data, char *message, const char *ermsg){
-	//where message gets assembled
+
+
+void clearlog(void) {
+	//releases storage for message and list
+	//need to release memory
+	headptr = NULL;
+	tailptr = NULL;
+	
+}
+
+char *getlog(void) {
+	//writes log into a string
+	//is called from savelog
+	char *message;
+	//need to allocate string long enough for list
+	
 	errno = 0; //set errno to 0
 	message[0] = '\0'; //set message to empty
 	strcat(message, program); //begin with program name
@@ -42,26 +65,70 @@ int buildmsg(data_t data, char *message, const char *ermsg){
 		perror("Log message failed to build");
 		return -1;
 	}else{
+		
 		return 0;
 	}
-	
-	
-}
-
-void clearlog(void) {
-	
-}
-
-char *getlog(void) {
-	return NULL;
+	return message;
 }
 
 int savelog(char *filename){
-	return 0;
+	//save messages to file
+	//use getlog to retrieve log as a string
+	char *messages = getlog();
+	if(messages != NULL){
+		//first open file
+		FILE *logfile;
+		//open in append mode
+		logfile = fopen(filename, "a");
+		if (logfile == NULL){
+			perror("Log file failed to open");
+			return -1;
+		}else{
+			//write messages to logfile
+			char ch;
+			int i=0;
+			while(messages[i] != '\0'){
+				//write to file one char at a time
+				ch = messages[i];
+				i++;
+				//checks for error while writing to file
+				if(fputc(ch, logfile) == 'EOF'){
+					//perror("Failed to write to log file");
+					fclose(logfile);
+					return -1;
+				}
+			}			
+			//close file
+			fclose(logfile);
+		}
+		return 0;
+	}
+	
 }
 
 int main(int argc, string argv[]){
 	
+	//set filename for log
+	char *filename = "logfile.txt";
+	//add code to rename if filename command is used
+	
+	//program name for log file
+	char *program = "./loglib: ";
+	
+	//integer value for log file
+	int nValue = 42;
+	//add code to change value if command is used
+	
+	
+	//calls to add messages
+	
+	//call to savelog to write to file
+	if(savelog(filename) != 0){
+		perror("Failed to write to log file");
+	}
+	
+	//call clearlog to empty list 
+	clearlog();
 	
 	return 0;
 }
